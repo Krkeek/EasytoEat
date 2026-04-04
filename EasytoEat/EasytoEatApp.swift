54c89537ab633b9c365a648ad45d0d2cb75869b7
Ahmad <ahmadhijazi_me@hotmail.com>
2026-04-04 18:42:52 +0200

    Initial Commit

---
+++ EasytoEat/EasytoEatApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasytoEatApp.swift
+//  EasytoEat
+//
+//  Created by Ahmad Hijazi on 04.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct EasytoEatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
